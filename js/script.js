4b91371628ea40d117e11c517fdfcbc966fe31c5
juliannascruggs <julianna@scrug.gs>
2014-10-22 21:27:09 -0500

    Got my totally sweet menus to load using JSON.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+            $(document).ready(function(){
+                $('#slides').slick({
+                   autoplay: true, 
+                   autoplaySpeed: 2000, 
+                   fade: true, 
+                   cssEase: 'linear' 
+                });
+            });
+
 $(function() {
 
     // TODO #1 Set an event listener to listen for clicks on each menu section
@@ -21,18 +30,18 @@ $(function() {
       event.preventDefault();
 
       // Assign the id of the clicked element (this) to a variable named id
-      var  = $( this );
-
+      var id = $( this ).attr("id");
+      console.log(id);
       // Remove the class 'is-active' from all menu item headings
-      $( '' ).removeClass( '' );
+      $( '.menu-section-item' ).removeClass( 'active' );
 
       // Add 'is-active' to this specific action that was clicked (this). is-active
       // provides the visual cue for what's active via CSS
-      $( this ).addClass( '' );
+      $( this ).addClass( 'active' );
 
       // Once you're started with TODO #2, call the getMenu function here,
       // passing id as the argument
-
+      getMenu(id);
     });
 
 
@@ -52,7 +61,7 @@ $(function() {
 
     function getMenu( course ) {
      // Use `$.getJSON` to get the menu for whatever menu heading was clicked
-     $.getJSON( './json/menu-' + course + '.json', function( json ) {
+     $.getJSON( 'json/menu-' + course + '.json', function( json ) {
        populateMenu( json );
        // Once you're started with TODO #3, call the populateMenu function here
        // and pass json as the argument
@@ -69,7 +78,7 @@ $(function() {
 
       // start a for loop that iterates through json.length
       // add json.length into this for loop code
-      for( var i = 0; i < ; i++ ){
+      for( var i = 0; i < json.length; i++ ){
         // wrap each section in a menu-group div
         html += '<div class="menu-group columns small-12 medium-4">';
         // append inside the menu-group div a h4 with the json section name in it
@@ -81,12 +90,12 @@ $(function() {
         // prints out each menu-item div
         for( var j = 0; j < json[i].content.length; j++ ) {
           // for each menu item in json[i].content, create a menu-item div
-          html += '<div class="menu-item">';
+          html += '<div class="menu-item clearfix">';
           // inside each menu-item div, create a div for dish, ingredients, and price
           // add json[i]content[j].THING where THING is dish, ingredient, price.
-          html += '<div class="menu-item-dish">' + json[i].content[j]. + '</div>';
-          html += '<p class="menu-item-ingredients">' + json[i].content[j]. + '</p>';
-          html += '<div class="menu-item-price">' + json[i].content[j]. + '</div>';
+          html += '<div class="menu-item-dish">' + json[i].content[j].dish + '</div>';
+          html += '<p class="menu-item-ingredients">' + json[i].content[j].ingredients + '</p>';
+          html += '<div class="menu-item-price">' + json[i].content[j].price + '</div>';
           html += '</div>';
         }
 
@@ -97,9 +106,8 @@ $(function() {
       $( '.menu-section-content' ).html( html );
     }
 
-
-
     // TODO #4 Call getMenu with a menu of your choice and set that menu's
     // header to active so that a menu is loaded with the page by default
 
+    getMenu('breakfast');
 });
